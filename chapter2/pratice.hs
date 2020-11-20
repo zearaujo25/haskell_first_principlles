3b5716dde49c1c1a29b379c61f82f300eb3240b9
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-11-20 12:52:40 -0300

    Chapter 2 exercises

---
+++ chapter2/pratice.hs
@@ -0,0 +1,21 @@
+mult1       = x * y
+    where x = 5
+          y = 6
+
+mult2       = x*3 + y
+    where x = 3
+          y = 1000      
+
+mult3       = x * 5
+    where x = 10 * 5 + y
+          y = 10    
+
+mult4       = z / x + y
+    where x = 7
+          y = negate x
+          z = y*10
+
+let x = 7
+    y = negate x
+    z = y * 10
+    in z / x + y
---
+++ chapter2/triple.hs
@@ -0,0 +1,2 @@
+triple x = x * 3
+addLast x = x + (x-1)
