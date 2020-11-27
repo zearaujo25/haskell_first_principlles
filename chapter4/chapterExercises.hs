4c2428eb6d5b54ab13f35b9f8f1998bd4b79128a
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2020-11-26 21:41:33 -0300

    Chapter 4 final exercises

---
+++ chapter4/chapterExercises.hs
@@ -0,0 +1,23 @@
+awesome = ["Papuchon", "curry", ":)"]
+also = ["Quake", "The Simons"]
+allAwesome = [awesome, also]
+
+pseudoLenght :: [a] -> Int
+pseudoLenght list = length list
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = reverse x == x
+
+myAbs :: Integer -> Integer
+myAbs x = if x<0
+           then negate x
+          else
+           x
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f first_tuple second_tuple = ((snd first_tuple,snd second_tuple),(fst first_tuple, fst second_tuple))
+
+
+x = (+)
+func :: [a] -> Int 
+func xs = w `x` 1
+   where w = length xs 
