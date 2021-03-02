d13b99f61b7d6a4efe19ac4c6dbf31048a4e01b2
Jose Lucas de Araujo Gomes Cordeiro <jcordeiro@uolinc.com>
2021-03-01 22:46:13 -0300

    Finishing ciphers project

--- chapter13/ciphers/app/Main.hs
+++ chapter13/ciphers/app/Main.hs
@@ -1,12 +1,28 @@
 module Main where
 
+import System.Exit (exitSuccess)
 import VigenereCipher
 import CeasarCipher
 
 main :: IO ()
 main = do 
-    putStrLn "Type the word to be ciphered"
+    putStrLn "Type the word to be ciphered or ceasar deciphered"
     word <- getLine
-    putStr "Type number key: "
-    key <- getLine
-    putStrLn (ceasarCipher word (read key :: Int))
+    putStrLn "Type 1 for Ceasar Cipher, 2 for Vigenere Cipher or 3 for ceasar decipher"
+    choice <- getLine
+    case (choice) of 
+        "1" -> do 
+                putStr "Type number key: "
+                key <- getLine
+                putStrLn (ceasarCipher word (read key :: Int))
+        "2" -> do 
+                putStr "Type word key: "
+                key <- getLine
+                putStrLn (vigenereCipher word key)
+        "3" -> do 
+                putStr "Type number key: "
+                key <- getLine
+                putStrLn (ceasarDecipher word  (read key :: Int))
+        _   -> do 
+                putStr "Not an available choice"
+                exitSuccess
